fea7934064867f71b4472e4982ad15b792213d40
Julien Bouland <bouland.julien@gmail.com>
2020-05-14 13:30:33 +0200

    json parsing method added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# puts "creating ingredients"
+# Ingredient.create!(name: "lemon")
+# Ingredient.create!(name: "ice")
+# Ingredient.create!(name: "mint leaves")
+# puts "ingredients created"
+file = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+ingredients = JSON.parse(file.read)
+puts "creating ingredients"
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+  puts "#{ingredient} created"
+end
