9966516c4f4432da59002ce94824de8cf2907128
Julien Bouland <bouland.julien@gmail.com>
2020-05-14 16:10:28 +0200

    Cocktail and Dose controller are done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails do # GardensController#...
+    resources :doses
+    resources :ingredients
+  end
+  resources :doses
+  resources :ingredients
+
 end
