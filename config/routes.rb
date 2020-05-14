6ee5c481aca2e4572864d20638f56dc58dcafa09
Julien Bouland <bouland.julien@gmail.com>
2020-05-14 19:22:17 +0200

    Features almost don need css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails do # GardensController#...
-    resources :doses
-    resources :ingredients
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    resources :reviews, only: [:create]
   end
-  resources :doses
-  resources :ingredients
-
+  resources :doses, only: [:destroy]
 end
